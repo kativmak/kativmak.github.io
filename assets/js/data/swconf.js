f249dccf206d6575e32ebfa114d2d423264a3527
kativmak <kativmak@gmail.com>
2025-07-17 23:00:42 +0200

    2d version

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,38 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1752506491',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/research/',
+      
+        '/publications/',
+      
+        '/CV/',
+      
+        '/contact/',
+      
+
+      
+      
+        '/assets/js/dist/categories.min.js',
+        '/assets/js/dist/commons.min.js',
+        '/assets/js/dist/home.min.js',
+        '/assets/js/dist/misc.min.js',
+        '/assets/js/dist/page.min.js',
+        '/assets/js/dist/post.min.js'
+    ],allowHosts: [
+      
+          'fonts.googleapis.com',
+          'fonts.gstatic.com',
+          'fonts.googleapis.com',
+          'cdn.jsdelivr.net',
+          'polyfill.io'
+      
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
+
